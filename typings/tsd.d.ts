17b4e02d82772617462dd9f35df139691903a1ce
Luke Czernal <lukeczernal@Lukes-MacBook-Pro.local>
2016-01-23 00:48:55 +0100

    typings removed

--- typings/tsd.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference path="jquery/jquery.d.ts" />
-/// <reference path="hammerjs/hammerjs.d.ts" />
-/// <reference path="touch-events/touch-events.d.ts" />
-/// <reference path="fastclick/fastclick.d.ts" />
-/// <reference path="lodash/lodash.d.ts" />
